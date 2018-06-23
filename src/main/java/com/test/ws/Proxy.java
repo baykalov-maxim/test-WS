4ce3f522e4d96b1890fdaadf62e78fff80528d85
baykalov-maxim <baykalov.maxim@gmail.com>
2018-06-23 11:58:23 +0300

    add WS support

--- src/main/java/com/test/ws/Proxy.java
+++ src/main/java/com/test/ws/Proxy.java
@@ -1,13 +1,36 @@
 package com.test.ws;
 
+import okhttp3.*;
+
 public class Proxy extends Server {
 
     public Proxy(int port) {
         super(port);
     }
 
+    OkHttpClient client = new OkHttpClient();
+
     @Override
     protected String handleRequest(String request) {
-        return "HTTP/1.0 200 OK\r\n\r\n";
+        Request.Builder builder = new Request.Builder().url("ws://echo.websocket.org");
+        WebSocket ws = client.newWebSocket(builder.build(), new WsListener());
+        ws.send("asd");
+
+        return "";
+    }
+
+    private class WsListener extends WebSocketListener {
+
+        @Override
+        public void onOpen(WebSocket webSocket, Response response) {
+            super.onOpen(webSocket, response);
+            System.out.println(response.code());
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, String text) {
+            super.onMessage(webSocket, text);
+            System.out.println("Received message: " + text);
+        }
     }
 }
